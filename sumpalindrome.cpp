7bed99b9aaf3338f911cc7804112a9dd1f1d9502
lavanya shukla <lavanyashukla0411@gmail.com>
2026-06-03 20:30:17 +0530

    sumofdigits palindrome

---
+++ sumpalindrome.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isSumPalindrome(int n) {
+        int sum = 0;
+        int temp = n;
+
+        // Find sum of digits
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+
+        // Check if sum is palindrome
+        int original = sum;
+        int reverse = 0;
+
+        while (sum > 0) {
+            reverse = reverse * 10 + sum % 10;
+            sum /= 10;
+        }
+
+        return original == reverse;
+    }
+};
+
+int main() {
+    Solution s;
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (s.isSumPalindrome(n))
+        cout << "Palindrome" << endl;
+    else
+        cout << "Not Palindrome" << endl;
+
+    return 0;
+}
