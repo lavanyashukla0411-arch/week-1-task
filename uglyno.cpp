3fadd22721b2a2343f5ee9604cc63cd90485c75b
lavanya shukla <lavanyashukla0411@gmail.com>
2026-06-03 21:03:30 +0530

    uglyno problem solved

---
+++ uglyno.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isUgly(int n) {
+
+    if (n <= 0)
+        return false;
+
+    while (n % 2 == 0)
+        n /= 2;
+
+    while (n % 3 == 0)
+        n /= 3;
+
+    while (n % 5 == 0)
+        n /= 5;
+
+    return n == 1;
+}
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    if (isUgly(n))
+        cout << "Ugly Number";
+    else
+        cout << "Not an Ugly Number";
+
+    return 0;
+}
