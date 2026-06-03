b0db002803531402a62be3b17576cebd492fde09
lavanya shukla <lavanyashukla0411@gmail.com>
2026-06-03 20:36:59 +0530

    count digits problem

---
+++ countdigits.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    int count = 0;
+
+    while (n > 0) {
+        count++;
+        n /= 10;
+    }
+    cout << "Number of digits = " << count;
+    return 0;
+}
